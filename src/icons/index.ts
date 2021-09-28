7797055ce3b62e2ed3d1c7b53d9adf4019d11f7f
valentin2106 <fernandoValentim.cja@hotmail.com>
2021-09-28 11:14:12 -0300

    icon-ticket

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,6 +2,9 @@ export { IconMail } from './Mail';
 export { IconBell } from './Bell';
 export { IconSearch } from './Search';
 export { IconDashboard } from './Dashboard';
+export { IconTicket } from './Ticket';
+
+
 
 
 
