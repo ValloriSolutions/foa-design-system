cb69f9cccbc48d3abfd1035dfd9c4ab12a18626d
valentin2106 <fernandoValentim.cja@hotmail.com>
2021-09-28 12:18:34 -0300

    change-iconedit

--- src/icons/Edit.tsx
+++ src/icons/Edit.tsx
@@ -12,7 +12,6 @@ export const IconEdit = ({
       viewBox="0 0 24 23"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      href="http://www.w3.org/1999/xlink"
     >
       <rect
         x="0.473633"
@@ -30,17 +29,9 @@ export const IconEdit = ({
           width="1"
           height="1"
         >
-          <use
-            href="#image0"
-            transform="translate(0.027026) scale(0.0315316 0.0333333)"
-          />
+          <use transform="translate(0.027026) scale(0.0315316 0.0333333)" />
         </pattern>
-        <image
-          id="image0"
-          width="30"
-          height="30"
-          href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAuklEQVRIie3VoQrCQAAG4A+x+xJisxnMghisRl9iyahN38Jq8Wl8AItVMVk16GAOhxN2N4T9cOF2cN/9HOxo8udJcMEBg1joCvfMuGIYG83ib81boU/ySgfb0EhR63MIaFUCT6pG0403ubVFZm0ZCk3Hp+b5b5WjRc2joMHwMmiUO23QBv05E8/39Bta6c8hzbwOFMZ1oNCLifZzG3cxwgy3UCisFbeahkLhKNI9tnPz0wvdYR8SbhItDwJbnBVPxJOqAAAAAElFTkSuQmCC"
-        />
+        <image id="image0" width="30" height="30" />
       </defs>
     </svg>
   );
