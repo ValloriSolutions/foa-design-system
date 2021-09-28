aab8db8a8342dbd53e5ed71262affd71d81ef4dc
valentin2106 <fernandoValentim.cja@hotmail.com>
2021-09-28 11:26:08 -0300

    icon-Filter

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -4,6 +4,7 @@ export { IconSearch } from './Search';
 export { IconDashboard } from './Dashboard';
 export { IconTicket } from './Ticket';
 export { IconCredit } from './Credit';
+export { IconFilter} from './Filter'
 
 
 
