4a576cddda22c481ee0e4f119e777c2e51eb8790
Amanda Natallie <amanda.natallie.2@gmail.com>
2021-10-15 01:27:16 -0300

    Add Avatar component & Stories

---
+++ stories/Avatar.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Avatar, AvatarProps } from '../src/components/Avatar';
+
+export default {
+  title: 'Components/Avatar',
+  component: Avatar,
+  parameters: {
+    controls: { expanded: true },
+  },
+} as Meta;
+
+const Template: Story<AvatarProps> = (props) => <Avatar {...props} />;
+
+export const Small = Template.bind({});
+Small.args = {
+  imageUrl: 'https://i.pravatar.cc/100?u=amanda1',
+  alt: 'Amanda',
+  size: 'sm',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  imageUrl: 'https://i.pravatar.cc/200?u=amanda2',
+  alt: 'Jackeline',
+  size: 'md',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  imageUrl: 'https://i.pravatar.cc/300?u=amanda3',
+  alt: 'Jackeline',
+  size: 'lg',
+};
+
+export const WithFallback = Template.bind({});
+WithFallback.args = {
+  imageUrl: 'https://invalidurl.invalid',
+  alt: 'Jackeline',
+  size: 'md',
+};
+
+export const WithFallbackNoAlt = Template.bind({});
+WithFallbackNoAlt.args = {
+  imageUrl: 'https://invalidurl.invalid',
+  size: 'md',
+};
